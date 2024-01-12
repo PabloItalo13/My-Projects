3954d26d9d626a5ba9241a40dc34a23c6e6ff49c
Italo Bentes <pabloitalobs@gmail.com>
2024-01-12 14:12:16 -0400

    Começando a implementar o tabuleiro

---
+++ ChessGame/ChessGame/Tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; set; }
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            this.Posicao = posicao;
+            this.Cor = cor;
+            this.Tabuleiro = tabuleiro;
+            this.QtdMovimentos = 0;
+        }
+    }
+}
